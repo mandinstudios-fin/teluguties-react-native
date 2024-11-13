3776b728c013e412788b3730642b9722fac4246e
dilsekhpatnaik7 <dpj020@nist.edu>
2024-11-13 10:02:00 +0530

    matches added

--- src/components/Matches/Matches.tsx
+++ src/components/Matches/Matches.tsx
@@ -3,8 +3,45 @@ import React, { useState } from 'react'
 import Header from '../Header/Header'
 import ProfileGrid from '../Profiles/ProfileGrid'
 
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
 const Matches = ({ navigation }) => {
   const [data, setData] = useState<any>([]);
+
+  const getMatchesForUser = async () => {
+    const currentUser = auth().currentUser;
+
+    if (currentUser) {
+      try {
+        const userDoc = await firestore().collection('users').doc(currentUser.uid).get();
+
+        if (userDoc.exists) {
+          const userDataFirestore = userDoc.data();
+          const { religion, caste } = userDataFirestore();
+
+          const querySnapshot = await firestore().collection('users')
+          .where('religion', '==', religion)
+          .where('caste', '==', caste)
+          .get();
+
+          const matchedUsers = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data }));
+          setData(matchedUsers);
+        } else {
+          console.log('No user data found for this UID');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    } else {
+      console.log('No user is currently authenticated.');
+    }
+  }
+
+  useEffect(() => {
+    getMatchesForUser();
+  }, [])
+
   return (
     <SafeAreaView style={styles.safearea}>
       <View style={styles.main}>
